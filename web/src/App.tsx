893309c5fe6bf171bd33b96d79b8ee4186271715
Josie Rice <josie@stocksy.com>
2021-12-30 15:16:49 -0800

    getting CORS error trying to access local graphql

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,22 @@
+import { gql, useQuery } from "@apollo/client";
+
+const RUSHING_STATS = gql`
+  query RushingStats {
+    totalCount
+  }
+`;
+
 const App = () => {
+  const { loading, error, data } = useQuery(RUSHING_STATS);
+
+  console.log({ loading });
+  console.log({ error });
+  console.log({ data });
+
   return (
     <div>
       <header>
-        <p>Basic React component rendering !!</p>
+        <p>Basic React component rendering</p>
       </header>
     </div>
   );
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,28 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const link = new HttpLink({
+  uri: "http://localhost:8080/graphql",
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
   document.getElementById("root")
 );
