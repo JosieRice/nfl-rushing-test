f37b91b89ddb303726120f152e3543ca3e6a8e45
Josie Rice <josie@stocksy.com>
2021-12-30 13:15:07 -0800

    added sorting and filter variables to graphql endpoint

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,9 +1,21 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+/** NOTE: should be migrated to a db, but I'm Frontend, so I'm just leaving as is */
 const rushingStats = require("./rushing.json");
 
 const schema = buildSchema(`
+  enum SortBy {
+    yds
+    touchDown
+    longest
+  }
+
+  enum OrderBy {
+    asc
+    desc
+  }
+
   type RushingStats  {
     player: String!
     team: String!
@@ -24,7 +36,13 @@ const schema = buildSchema(`
 
   type Query {
     totalCount: Int!
-    rushingStats(numPerPage: Int pgNum:Int): [RushingStats]
+    rushingStats(
+      numPerPage: Int!
+      pgNum:Int!
+      filter: String
+      sortBy: SortBy
+      orderBy: OrderBy
+    ): [RushingStats]
   }
 `);
 
@@ -33,19 +51,31 @@ const rootValue = {
     return rushingStats.length;
   },
 
-  /**
-   * this is a naive implementation because I always have all of the rushingStats to sort and paginate
-   * the args provided would be enough to get a limit on a sql query and query specific data, which would be the scalable solution
-   * I'm choosing to not go through setting up a sql db in a container and migrate the data though, because I'm applying for a frontend job and it feels like putting effort in the wrong direction
-   */
   rushingStats: (args) => {
-    const sanitizedRushingStats = rushingStats.map((stat) => {
+    /**
+     * filter logic
+     * NOTE: this should be part of a db query - but I'm Frontend, so I'm just cheating it
+     */
+    let filteredStats = rushingStats;
+    if (args.filter) {
+      filteredStats = rushingStats.map((stat) => {
+        const player = stat.Player;
+        if (player.includes(args.filter)) {
+          return stat;
+        } else {
+          return null;
+        }
+      });
+    }
+    const filteredStatsClean = filteredStats.filter(Boolean);
+
+    /**
+     * graphql schema doesn't support some of the characters in the keys provided, so this is linking them back up for the graphql response
+     * javascript code is usually in camelcase, so I'm using this resolver to rename some keys since React will be consuming this API
+     */
+    const renamedStats = filteredStatsClean.map((stat) => {
       let sanitizedStat = { ...stat };
 
-      /**
-       * graphql schema doesn't support some of the characters in the keys provided, so this is linking them back up for the graphql response
-       * javascript code is usually in camelcase, so I'm using this resolver to rename some keys since React will be consuming this API
-       */
       sanitizedStat.player = stat.Player;
       sanitizedStat.team = stat.Team;
       sanitizedStat.pos = stat.Pos;
@@ -77,10 +107,45 @@ const rootValue = {
       return sanitizedStat;
     });
 
+    /**
+     * sort logic
+     * NOTE: yet again, this should be part of the db query to return the correct info, but I'm Frontend so I'm going to forego doing it
+     */
+    let sortedStats = renamedStats;
+    if (args.sortBy && args.orderBy) {
+      const longestAsc = args.sortBy === "longest" && args.orderBy === "asc";
+      const longestDesc = args.sortBy === "longest" && args.orderBy === "desc";
+      const touchDownAsc =
+        args.sortBy === "touchDown" && args.orderBy === "asc";
+      const touchDownDesc =
+        args.sortBy === "touchDown" && args.orderBy === "desc";
+      const ydsAsc = args.sortBy === "yds" && args.orderBy === "asc";
+      const ydsDesc = args.sortBy === "yds" && args.orderBy === "desc";
+
+      if (longestAsc) {
+        sortedStats.sort((a, b) => a.longest - b.longest);
+      } else if (longestDesc) {
+        sortedStats.sort((a, b) => b.longest - a.longest);
+      } else if (touchDownAsc) {
+        sortedStats.sort((a, b) => a.touchDown - b.touchDown);
+      } else if (touchDownDesc) {
+        sortedStats.sort((a, b) => b.touchDown - a.touchDown);
+      } else if (ydsAsc) {
+        sortedStats.sort((a, b) => a.yds - b.yds);
+      } else if (ydsDesc) {
+        sortedStats.sort((a, b) => b.yds - a.yds);
+      }
+    }
+
+    /**
+     * pagination logic
+     * NOTE: should be a db query with a limit to allow for scaling up data - but I'm Frontend, so I'm just cheating it
+     */
     const startingIndex = (args.pgNum - 1) * args.numPerPage;
     const endingIndex = startingIndex + args.numPerPage;
+    let paginatedStats = sortedStats.slice(startingIndex, endingIndex);
 
-    return sanitizedRushingStats.slice(startingIndex, endingIndex);
+    return paginatedStats;
   },
 };
 
