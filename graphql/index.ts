14993d9a7e6712e466dba4c8edb78325913feb67
Josie Rice <josie@stocksy.com>
2021-12-29 16:33:05 -0800

    added types for rushing stats

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,23 +1,49 @@
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require('graphql')
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+const rushingStats = require("./rushing.json");
 
-const schema = buildSchema(`type Query { hello: String }`)
-const rootValue = { hello: () => 'Hello from Express GraphQL!' }
+const schema = buildSchema(`
+  type RushingStats {
+    Player: String!
+    Team: String!
+    Pos: String!
+    Att: Int
+    Att_G: Float
+    Yds: Int
+    Avg: Float
+    Yds_G: Float
+    TD: Int
+    Lng: String
+    First: Int
+    First_Percent: Float
+    Twenty_Plus: Int
+    Fourty_Plus: Int
+    FUM: Int
+  }
 
-const app = express()
+  type Query {
+    rushingStats: [RushingStats]
+  }
+`);
+
+const rootValue = {
+  rushingStats: () => {
+    return rushingStats;
+  },
+};
+
+const app = express();
 
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema,
     rootValue,
-    graphiql: {
-      headerEditorEnabled: true
-    },
-  }),
-)
+    graphiql: true,
+  })
+);
 
-app.listen(8080, '0.0.0.0')
+app.listen(8080, "0.0.0.0");
 
-console.log('Running Express GraphQL server at http://localhost:8080/graphql')
+console.log("Running Express GraphQL server at http://localhost:8080/graphql");
